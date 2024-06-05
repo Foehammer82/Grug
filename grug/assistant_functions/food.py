c0f28eb944dea008176c881ea36d4afa6f7d87ae
Foehammer82 <blakej42@live.com>
2024-06-05 13:17:06 -0500

    add todo item to food ai tool

--- grug/assistant_functions/food.py
+++ grug/assistant_functions/food.py
@@ -1,3 +1,7 @@
+# TODO: before we can do any of this, we need to setup the app to have a dedicated assistant per group so that
+#       group assistant tools can be group aware.  at least without relying on the assistant to be aware and provide
+#       what group it's responding to
+
 # TODO: add functions to:
 #       - send food reminder
 #       - get food history
